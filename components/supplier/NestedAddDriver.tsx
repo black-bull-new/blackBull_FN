c97e5600619e6d6a7203e2de183544e72a5dc7ee
kaif-epicglobal <mohd.kaif@epicglobal.co.in>
2024-02-28 11:11:42 +0530

    working on add driver in supplier

--- components/supplier/NestedAddDriver.tsx
+++ components/supplier/NestedAddDriver.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Progressbar from "../Progressbar";
 import Image from "next/image";
 import Maininputfield from "../Maininputfield";
@@ -13,6 +13,119 @@ const NestedAddDriver = (props: any) => {
   const { addDriver, setAddDriver, error, setError } = props;
   console.log("AddDriver state", addDriver);
   console.log("Error State", error);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [profile, setProfile] = React.useState("");
+  const [selectedProfile, setSelectedProfile] = React.useState("");
+  const [selectedUploadRegoDocument, setSelectedUploadRegoDocument] =
+    React.useState("");
+  const [documentRender, setDocumentRender] = React.useState("");
+  const [urls, setUrls] = useState<string[]>([]);
+  const [selectedFiles, setSelectedFiles] = useState<
+  { id: number; file: File; currentDate: Date | null }[]
+>([]);
+
+const [uploadStatus, setUploadStatus] = useState<{ [id: number]: boolean }>(
+  {}
+);
+const [showUploadMessage, setShowUploadMessage] = useState(false);
+
+const modifiedUrls = urls.reduce((acc: any, url, index) => {
+  acc[index + 1] = url;
+  return acc;
+}, []);
+
+  const handleUploadClick: any = () => {
+    if (fileInputRef.current) {
+      fileInputRef?.current?.click();
+    }
+  };
+
+  const handleFileChanges = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    documentId: number
+  ) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    const documentExists = documentCollectionData.find(
+      (doc) => doc.id === documentId
+    );
+    if (file && documentExists) {
+      const newSelectedFiles = [...selectedFiles];
+      const existingFileIndex = newSelectedFiles.findIndex(
+        (file) => file.id === documentId
+      );
+      const currentDate = new Date();
+      if (existingFileIndex !== -1) {
+        newSelectedFiles[existingFileIndex] = {
+          id: documentId,
+          file,
+          currentDate,
+        };
+      } else {
+        newSelectedFiles.push({ id: documentId, file, currentDate });
+      }
+      setSelectedFiles(newSelectedFiles);
+    }
+  };
+
+  const handleFileChange = (setSide: any, setPreview: any) => (event: any) => {
+    const selectedFile = event.target.files && event.target.files[0];
+    setSide({ file: selectedFile });
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader?.result! as any);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  const handleProfileFileChange = handleFileChange(
+    setSelectedProfile,
+    setProfile
+  );
+
+  const handleDocumentUpload = handleFileChange(
+    setSelectedUploadRegoDocument,
+    setDocumentRender
+  );
+
+  const handleUploadFileWithId = async (id: number, combinedObject: any) => {
+    try {
+      const project = combinedObject[id];
+      if (id && project?.id) {
+        const file = [project?.file];
+        const uploadDocumentResponses = await Promise.all(
+          Object.values(file)?.map((file) =>
+            uploadSingleSingleDriverOnboardingDocuments(file)
+          )
+        );
+        const newUrls = uploadDocumentResponses
+          ?.map((response) => response?.response)
+          .filter(Boolean);
+        setUrls((prevUrls) => {
+          const updatedUrls = [...prevUrls];
+          updatedUrls[id - 1] = newUrls[0]; // Update the URL at the correct index
+          return updatedUrls;
+        });
+        setUploadStatus((prevStatus) => ({ ...prevStatus, [id]: true }));
+        setTimeout(() => {
+          setShowUploadMessage(true);
+        }, 4000);
+      }
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+  };
+
+  const handleViewDocuments = (id: number) => {
+    const index = id;
+    if (index >= 1 && index < modifiedUrls.length) {
+      const url = modifiedUrls[index];
+      window.open(url, "_blank");
+    } else {
+      console.error("URL not found for id:", id);
+    }
+  };
 
   return (
     <div>
@@ -21,10 +134,51 @@ const NestedAddDriver = (props: any) => {
       </div>
       <div className="font-semibold text-xl mt-4 bg-white p-4 mr-4 rounded-md mb-20">
         <Progressbar />
-        <div className="relative w-fit mt-4">
-          <Image src="/driverImage.svg" alt="driver" width={100} height={100} />
-          <span className="w-6 h-6 rounded-full bg-accent3 block text-white flex justify-center items-end text-xl absolute right-2 bottom-2">
-            +
+        <div className="relative w-fit">
+          <span className="flex flex-row justify-center my-4">
+            <span className="mb-4 text-center flex justify-center items-center">
+              <label htmlFor="profilelabel">
+                <div
+                  className="w-[100px]  rounded-full h-[100px] cursor-pointer"
+                  onChange={handleUploadClick}
+                >
+                  {profile ? (
+                    <div className="w-full h-full">
+                      <Image
+                        src={profile}
+                        alt="driver"
+                        width={100}
+                        className="w-[100px] h-[100px] border rounded-full"
+                        height={100}
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      <Image
+                        src="/driverImage.svg"
+                        alt="driver"
+                        width={100}
+                        height={100}
+                        className="w-[100px] h-[100px]"
+                      />
+                      <span className="w-6 h-6 rounded-full bg-accent3 block text-white flex justify-center items-end text-xl absolute right-2 bottom-6">
+                        +
+                      </span>
+                    </>
+                  )}
+                </div>
+              </label>
+              <span className="text-sm">
+                {" "}
+                <input
+                  id="profilelabel"
+                  type="file"
+                  style={{ display: "none" }}
+                  ref={fileInputRef}
+                  onChange={(e) => handleProfileFileChange(e)}
+                />
+              </span>
+            </span>
           </span>
         </div>
         <h2 className="text-black font-semibold p-4 mt-6">
@@ -749,7 +903,12 @@ const NestedAddDriver = (props: any) => {
               });
             }}
           />
-          <FileUpload file="Choose License Document " />
+          <FileUpload
+            file="Upload Rego Document"
+            onChange={handleDocumentUpload}
+            // @ts-expect-error
+            fileName={selectedUploadRegoDocument?.file?.name || ""}
+          />
         </div>
         {/* <h2 className="text-black font-semibold p-4 mt-4">
           Employment History
@@ -796,55 +955,166 @@ const NestedAddDriver = (props: any) => {
             errorMessage={error.specialDrivingLicenseError}
           />
         </div>
-        <div className="mb-4 mt-8">
-          <h3 className="text-black w-full mb-6 rounded-md font-semibold pl-4">
-            {" "}
-            Onboarding Documents
-          </h3>
+        <div className="bg-white mr-4 px-4 rounded-md mt-4 p-4">
+          <div className="mb-4 mt-8">
+            <h3 className="w-full mb-4 rounded-md font-semibold text-black">
+              {" "}
+              Onboarding Documents
+            </h3>
 
-          <div className="text-black grid grid-cols-5 bg-table-header p-4 rounded-md text-center mb-2">
-            {documentCollectionHeadingDriver?.map((value, index) => {
-              return (
-                <>
-                  <h4 key={index} className="font-semibold text-sm">
-                    {value.heading}
-                  </h4>
-                </>
-              );
-            })}
-          </div>
-          <div className="text-black grid grid-cols-5 p-4 rounded-md text-center font-normal text-[16px] items-center">
-            {documentCollectionDataDriver.map((value, index) => {
-              return (
-                <>
-                  <div className="mb-6">{value.documentType}</div>
-                  <div className="text-center ">
-                    <Button
-                      text="Upload"
-                      className="!w-fit m-auto bg-[#2B36D9] px-6 rounded-full mb-6 py-[4px]"
-                    />
-                  </div>
-                  <div className="mb-6">{value.uploadedDocument}</div>
-                  <div className="mb-6">{value.uploadDate}</div>
-                  <div className="mb-6 flex gap-2 justify-center">
-                    <Image
-                      src={"/edit.svg"}
-                      alt="svg"
-                      width={24}
-                      height={24}
-                      className="cursor-pointer"
-                    />
-                    <Image
-                      src={"/trash.svg"}
-                      alt="svg"
-                      width={24}
-                      height={24}
-                      className="cursor-pointer"
-                    />
-                  </div>
-                </>
-              );
-            })}
+            <div className="grid grid-cols-5 bg-table-header p-4 rounded-md text-black text-center mb-2 ">
+              {documentCollectionHeading?.map((value, index) => {
+                return (
+                  <>
+                    <h4 key={index} className="font-semibold text-sm">
+                      {value.heading}
+                    </h4>
+                  </>
+                );
+              })}
+            </div>
+            <div className="grid grid-cols-5 p-4 rounded-md text-black text-center items-center">
+              {documentCollectionData?.map((data, index) => {
+                return (
+                  <>
+                    <div className="mb-6 align-middle">{data.documentType}</div>
+                    <div className="text-center mb-6">
+                      <label className="cursor-pointer">
+                        <React.Fragment>
+                          {selectedFiles.find((file) => file.id === data?.id)
+                            ?.file ? (
+                            <div>
+                              <p>
+                                {
+                                  selectedFiles.find(
+                                    (file) => file.id === data?.id
+                                  )?.file.name
+                                }
+                              </p>
+                            </div>
+                          ) : (
+                            <span className="!w-fit m-auto bg-[#2B36D9] py-2 rounded-full text-sm px-6 mb-6 font-semibold  text-white">
+                              Select
+                            </span>
+                          )}
+                        </React.Fragment>
+                        <input
+                          type="file"
+                          className="hidden"
+                          accept=".doc,.docx,.pdf"
+                          onChange={(e) => handleFileChanges(e, data?.id)}
+                        />
+                      </label>
+                    </div>
+                    <div>
+                      {uploadStatus[data?.id] ? (
+                        <p style={{ color: "green" }}>
+                          {showUploadMessage ? (
+                            <div className="mb-6 underline decoration-[#2B36D9] text-center">
+                              <span
+                                className="cursor-pointer text-primary"
+                                onClick={() => handleViewDocuments(data?.id)}
+                              >
+                                View
+                              </span>
+                            </div>
+                          ) : (
+                            <span className="!w-fit m-auto bg-[#2B36D9] py-2 rounded-full cursor-pointer text-sm px-6 mb-6 font-semibold text-white">
+                              Uploading...
+                            </span>
+                          )}
+                        </p>
+                      ) : (
+                        <React.Fragment>
+                          {selectedFiles.find((file) => file.id === data?.id)
+                            ?.file ? (
+                            <div>
+                              <p className="!w-fit m-auto  cursor-pointer text-sm px-6  mb-6 font-semibold py-[4px] text-white">
+                                <span
+                                  className="!w-fit m-auto bg-[#2B36D9] py-2 rounded-full cursor-pointer text-sm px-6 mb-6 font-semibold  text-white"
+                                  onClick={() =>
+                                    handleUploadFileWithId(
+                                      data?.id,
+                                      combinedObject
+                                    )
+                                  }
+                                >
+                                  Upload
+                                </span>
+                              </p>
+                            </div>
+                          ) : (
+                            <p className="mb-6">No file Uploaded</p>
+                          )}
+                        </React.Fragment>
+                        // <span
+                        //   className="!w-fit m-auto bg-accent3 cursor-pointer text-sm px-6 rounded-md mb-6 font-semibold rounded-md py-[4px] text-white"
+                        //   onClick={() =>
+                        //     handleUploadFileWithId(data?.id, combinedObject)
+                        //   }
+                        // >
+                        //   Upload
+                        // </span>
+                      )}
+                    </div>
+                    {/* <div className="mb-6 align-middle mt-3">
+                        <React.Fragment>
+                          {selectedFiles.find((file) => file.id === data?.id)
+                            ?.file ? (
+                            <div>
+                              <p className="!w-fit m-auto bg-accent3 cursor-pointer text-sm px-6 rounded-md mb-6 font-semibold rounded-md py-[4px] text-white">
+                                <span>Upload&nbsp;</span>
+                                {
+                                  selectedFiles.find(
+                                    (file) => file.id === data?.id
+                                  )?.file.name
+                                }
+                              </p>
+                            </div>
+                          ) : (
+                            <p>No file Uploaded</p>
+                          )}
+                        </React.Fragment>
+                      </div> */}
+                    <div className="mb-6">
+                      <div>
+                        {selectedFiles.find((file) => file.id === data?.id) ? (
+                          <div>
+                            <p>
+                              {selectedFiles.find(
+                                (file) => file.id === data?.id
+                              )?.currentDate
+                                ? formatDate(
+                                    selectedFiles.find(
+                                      (file) => file.id === data?.id
+                                    )?.currentDate
+                                  )
+                                : "No date available"}
+                            </p>
+                          </div>
+                        ) : (
+                          <p>No date available</p>
+                        )}
+                      </div>
+                    </div>
+                    <div className="mb-6 flex gap-2 justify-center">
+                      <Image
+                        src={"/edit.svg"}
+                        alt="svg"
+                        width={24}
+                        height={24}
+                      />
+                      <Image
+                        src={"/trash.svg"}
+                        alt="svg"
+                        width={24}
+                        height={24}
+                      />
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
@@ -854,65 +1124,75 @@ const NestedAddDriver = (props: any) => {
 
 export default NestedAddDriver;
 
-const documentCollectionDataDriver = [
+const documentCollectionData = [
   {
+    id: 1,
     documentType: "Visa Status",
     uploadedDocument: "visa-status.pdf",
     uploadDate: "20/12/2023",
   },
   {
+    id: 2,
     documentType: "Driver License (Front) ",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 3,
     documentType: "Driver License (Back) ",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 4,
     documentType: "License History",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 5,
     documentType: "Police Verification",
     uploadedDocument: "police-verification.pdf",
     uploadDate: "20/12/2023",
   },
   {
+    id: 6,
     documentType: "Passport (Front)",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 7,
     documentType: "Passport (Back)",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 8,
     documentType: "Health Insurance",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 9,
     documentType: "Driver Certificate",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 10,
     documentType: "Fitness",
     uploadedDocument: "-",
     uploadDate: "-",
   },
   {
+    id: 11,
     documentType: "Drug Test",
     uploadedDocument: "-",
     uploadDate: "-",
   },
 ];
-
-const documentCollectionHeadingDriver = [
+const documentCollectionHeading = [
   {
     heading: "Document type",
   },
--- src/pages/onboarding/add-supplier/index.tsx
+++ src/pages/onboarding/add-supplier/index.tsx
@@ -150,7 +150,7 @@ const AddSupplier = () => {
   const step1Btn = "Proceed to Add Vehicle";
   const step2Btn = "Proceed to Add Driver";
   const step3Btn = "Submit";
-  const [buttonState, seButtonState] = useState(step2Btn);
+  const [buttonState, seButtonState] = useState(step3Btn);
   /**
    * add supplier state and its error state
    */
