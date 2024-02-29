04453cb0239ee8710f820b697c8a8255a75a888b
AasifAahaan <aasifalvi8888@gmail.com>
2024-02-29 11:08:45 +0530

    Changes

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,32 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FileUpload = (props: any) => {
+const FileUpload = ({ fileName, file, onChange, errorMessage }: any) => {
   return (
     <div className="relative w-full">
       <div>
         <label
-          htmlFor={props?.id}
+          htmlFor="myFile"
           className="flex gap-2 items-center cursor-pointer"
         >
           <div className="bg-[#2B36D9] text-white px-10 py-[12px] flex justify-center text-sm font-semibold rounded-full whitespace-nowrap ">
-            {props?.fileName !== "" ? props?.fileName : (props?.file !== "" ? props?.file : "Browse")}
+            {fileName !== "" ? fileName : (file !== "" ? file : "Browse")}
           </div>
         </label>
         <input
           type="file"
-          id={props?.id}
-          name={props?.name}
+          id="myFile"
+          name="filename"
           hidden
-          onChange={props?.onChange}
+          onChange={onChange}
         />
       </div>
-      {props?.errorMessage && (
+      {errorMessage && (
         <div className="text-red-500 text-[12px] mt-1">
-          {props?.errorMessage}
+          {errorMessage}
         </div>
       )}
     </div>
   );
 };
-
 export default FileUpload;
